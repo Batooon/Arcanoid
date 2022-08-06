e21e646f7785ceac0e8a7938aa70c5655ef2ebcf
Anton <rozum.ant@gmail.com>
2022-08-06 23:47:16 +0300

    instantiating first ball from pool

--- Assets/Resources Static/Scripts/Entry.cs
+++ Assets/Resources Static/Scripts/Entry.cs
@@ -4,9 +4,19 @@ public class Entry : MonoBehaviour
 {
     [SerializeField] private BlocksGenerator _blocksGenerator;
     [SerializeField] private Transform _blocksParent;
+    [SerializeField] private Ball _ballPrefab;
+    [SerializeField] private Transform _ballSpawnPoint;
+
+    private ObjectPool<Ball> _ballPool;
 
     private void Start()
     {
         _blocksGenerator.SpawnBlocks(_blocksParent);
+        _ballPool = new ObjectPool<Ball>(_ballPrefab);
+
+        var spawnedBall = _ballPool.GetAvailableObject();
+        spawnedBall.transform.position = _ballSpawnPoint.position;
+        spawnedBall.Activate();
+        spawnedBall.gameObject.SetActive(true);
     }
 }
