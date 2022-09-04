45b8239694f918945b97bd6f7917624d0406233b
Anton <rozum.ant@gmail.com>
2022-09-04 21:20:50 +0300

    calling powerups spawner when destroyed

--- Assets/Resources Static/Scripts/Block.cs
+++ Assets/Resources Static/Scripts/Block.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer _renderer;
+    private PowerupsSpawner _spawner;
+
+    public void Init(PowerupsSpawner spawner)
+    {
+        _spawner = spawner;
+    }
     
     public void Destroy()
     {
+        _spawner.TrySpawn(transform.position);
         Destroy(gameObject);
     }
 
