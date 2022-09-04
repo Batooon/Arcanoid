73d25b1c583871c55e611fe405f6ef7d2d2a23f2
Anton <rozum.ant@gmail.com>
2022-09-04 21:20:03 +0300

    basic powerups spawner implemented

---
+++ Assets/Resources Static/Scripts/Ball/PowerupsSpawner.cs
@@ -0,0 +1,21 @@
+﻿using Platform;
+using UnityEngine;
+
+public class PowerupsSpawner : MonoBehaviour
+{
+    [SerializeField] private Powerup[] _powerups;
+
+    public void TrySpawn(Vector2 position)
+    {
+        for (var i = 0; i < _powerups.Length; i++)
+        {
+            var powerup = _powerups[i];
+            if (Random.value <= powerup.GetSpawnChance())
+            {
+                var spawnedPowerup = Instantiate(powerup, position, powerup.transform.rotation);
+                spawnedPowerup.StartMovement();
+                return;
+            }
+        }
+    }
+}
